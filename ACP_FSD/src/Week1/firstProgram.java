f7d2682b8ec51890bcadb979bcb325bed13e680e
DELL <DELL@DESKTOP-60RHCE3>
2022-05-16 22:54:35 +0530

    First program

---
+++ ACP_FSD/src/Week1/firstProgram.java
@@ -0,0 +1,11 @@
+package Week1;
+
+public class firstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first program in java!!");
+
+	}
+
+}
